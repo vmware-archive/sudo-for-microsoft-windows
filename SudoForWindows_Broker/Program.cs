b580cdf99749f234b94c52bc7bdfd55d4a5a014a
Jeremy Banker <jeremyb@vmware.com>
2023-10-10 09:50:44 -0600

    Initial Commit of Sudo For Microsoft Windows product
    Signed-off-by: Jeremy Banker <jeremyb@vmware.com>

---
+++ SudoForWindows_Broker/Program.cs
@@ -0,0 +1,57 @@
+﻿//Copyright 2023 VMware, Inc.
+//SPDX-License-Identifier: BSD-2-Clause
+using System;
+using System.Reflection;
+using System.ServiceProcess;
+using System.Threading;
+
+namespace SudoForWindows_Broker
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///     The main entry point for the application.
+        /// </summary>
+        private static void Main(string[] args)
+        {
+            ServiceBase ServiceToRun = new SudoBroker(args);
+            if (Environment.UserInteractive)
+                RunInteractive(ServiceToRun);
+            else
+                ServiceBase.Run(ServiceToRun);
+        }
+
+        private static void RunInteractive(ServiceBase service)
+        {
+            Console.WriteLine("Service running in interactive mode.");
+            Console.WriteLine();
+
+#pragma warning disable S3011 // Reflection should not be used to increase accessibility of classes, methods, or fields
+            var onStartMethod = typeof(ServiceBase).GetMethod("OnStart",
+                BindingFlags.Instance | BindingFlags.NonPublic);
+#pragma warning restore S3011 // Reflection should not be used to increase accessibility of classes, methods, or fields
+            Console.Write("Starting {0}...", service.ServiceName);
+            onStartMethod.Invoke(service, new object[] { new string[] { } });
+            Console.Write("Started");
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine(
+                "Press any key to stop the service and end the process...");
+            Console.ReadKey();
+            Console.WriteLine();
+
+#pragma warning disable S3011 // Reflection should not be used to increase accessibility of classes, methods, or fields
+            var onStopMethod = typeof(ServiceBase).GetMethod("OnStop",
+                BindingFlags.Instance | BindingFlags.NonPublic);
+#pragma warning restore S3011 // Reflection should not be used to increase accessibility of classes, methods, or fields
+            Console.Write("Stopping {0}...", service.ServiceName);
+            onStopMethod.Invoke(service, null);
+            Console.WriteLine("Stopped");
+
+            Console.WriteLine("All services stopped.");
+            // Keep the console alive for a second to allow the user to see the message.
+            Thread.Sleep(1000);
+        }
+    }
+}
---
+++ SudoForWindows_Shared/PipeFormatter.cs
@@ -0,0 +1,44 @@
+﻿//Copyright 2023 VMware, Inc.
+//SPDX-License-Identifier: BSD-2-Clause
+using System;
+using System.IO;
+using System.IO.Pipes;
+
+namespace SudoForWindows_Shared
+{
+    public class PipeFormatter
+    {
+        private static readonly Lazy<PipeFormatter> lazy =
+            new Lazy<PipeFormatter>(() => new PipeFormatter());
+
+        private PipeFormatter()
+        {
+        }
+
+        public static PipeFormatter Instance => lazy.Value;
+
+        public static void Serialize(PipeStream stream, PipeEnabledObject obj)
+        {
+                obj.WriteToStream(stream);
+        }
+
+        public static T Deserialize<T>(PipeStream stream) where T : PipeEnabledObject, new()
+        {
+            var messageBuffer = new byte[1024];
+            int bytes;
+            using (var memory = new MemoryStream())
+            {
+                do
+                {
+                    bytes = stream.Read(messageBuffer, 0, messageBuffer.Length);
+                    memory.Write(messageBuffer, 0, bytes);
+                } while (!stream.IsMessageComplete);
+
+                memory.Seek(0, SeekOrigin.Begin);
+                var obj = new T();
+                obj.LoadFromBytes(memory.ToArray());
+                return obj;
+            }
+        }
+    }
+}
